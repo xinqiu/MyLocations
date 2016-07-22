de475ee2360be13e08232b564beb75fe5ee24994
xinqiu <xinqiu.94@gmail.com>
2016-07-22 22:19:52 +0800

    开始看看iOS.Apprentice 3 MyLocations

---
+++ MyLocations/CurrentLocationViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.swift
+//  MyLocations
+//
+//  Created by Xin Qiu on 16/7/22.
+//  Copyright © 2016年 Xin Qiu. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+    
+    @IBAction func getLocation() {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
