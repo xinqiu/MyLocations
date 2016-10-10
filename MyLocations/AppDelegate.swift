8a315ac4029f09aa59869bcb42fbb6ca5227494c
xinqiu <xinqiu.94@gmail.com>
2016-10-10 23:27:42 +0800

    展示数据

--- MyLocations/AppDelegate.swift
+++ MyLocations/AppDelegate.swift
@@ -81,6 +81,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let tabBarControllers = tabBarController.viewControllers {
             let currentLocationViewController = tabBarControllers[0] as! CurrentLocationViewController
             currentLocationViewController.managedObjectContext = managedObjectContext
+            let navigationController = tabBarControllers[1] as! UINavigationController
+            let locationsViewController = navigationController.viewControllers[0] as! LocationsViewController
+            locationsViewController.managedObjectContext = managedObjectContext
         }
         return true
     }
